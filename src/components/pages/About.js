d8e5d21b29dc836b8105e7f47279a0ef04af0cc6
dreadedharvester <tarunkhare54321@gmail.com>
2019-08-09 16:08:24 +0530

    added project

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h1>
+                <p>This Todo app is created by Tarun Khare.</p>
+            </h1>
+        </React.Fragment>
+    )
+}
